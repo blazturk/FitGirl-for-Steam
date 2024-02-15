c80a891fccad9a174ed959d3df23d0bdcd25dcb0
blazturk <blazturk2003@gmail.com>
2024-02-15 19:00:18 +0100

    Updated search system & added style to button

--- content_script.js
+++ content_script.js
@@ -1,5 +1,36 @@
 // content_script.js
 
+// Function to inject custom CSS
+function injectCustomCSS() {
+    const css = `
+        /* Define styles for the custom button */
+        .custom_button {
+            background: linear-gradient(to right, rgb(182, 244, 146), rgb(51, 139, 147));
+            border: none;
+            color: white;
+            padding: 10px 20px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 4px 2px;
+            cursor: pointer;
+            border-radius: 5px;
+        }
+
+        .custom_button:hover {
+            background-color: #45a049; /* Darker Green */
+        }
+    `;
+
+    // Create a <style> element
+    const styleElement = document.createElement('style');
+    styleElement.textContent = css;
+
+    // Append the <style> element to the document's <head>
+    document.head.appendChild(styleElement);
+}
+
 // Function to inject your custom button
 function injectCustomButton() {
     // Find the buy button
@@ -18,11 +49,9 @@ function injectCustomButton() {
         customButton.addEventListener('click', function() {
             // Get the game information
             let gameTitle = document.querySelector('.apphub_AppName').innerText;
-            gameTitle = gameTitle.toLowerCase(); // Convert to lowercase
-            gameTitle = gameTitle.replace(/\s+/g, '-');
 
             // Construct the URL for the page based on the game name
-            const pageUrl = `https://fitgirl-repacks.site/${encodeURIComponent(gameTitle)}`;
+            const pageUrl = `https://fitgirl-repacks.site/?s=${encodeURIComponent(gameTitle)}`;
 
             // Open a new tab/window with the URL
             window.open(pageUrl, '_blank');
@@ -30,5 +59,6 @@ function injectCustomButton() {
     }
 }
 
-// Call the function to inject your custom button when the page is loaded
+// Call the functions to inject custom CSS and custom button when the page is loaded
+injectCustomCSS();
 injectCustomButton();
